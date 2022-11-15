86b3e25ca61044b92b287c37483a8313f1440f7f
Natalie Murphy <Natalie.murphy@igrafx.com>
2022-11-15 12:07:33 -0800

    getting response from api

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
-import TopStories from './TopStories';
+import React from "react";
+import "../App.css";
+import TopStories from "./TopStories";
 
 function App() {
   return (
     <React.Fragment>
-      < TopStories />
+      <TopStories />
     </React.Fragment>
   );
 }
--- src/components/TopStories.js
+++ src/components/TopStories.js
@@ -1,9 +1,50 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 
-function TopStories(){
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [TopStories, setTopStories] = useState([]);
+function TopStories() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [topStories, setTopStories] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.nytimes.com/svc/topstories/v2/world.json?api-key=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`${response.status}: ${response.statusText}`);
+        } else {
+          return response.json();
+        }
+      })
+      .then((jsonifiedResponse) => {
+        setTopStories(jsonifiedResponse.results);
+        setIsLoaded(true);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoaded(true);
+      });
+  }, []);
+
+  if (error) {
+    return <h1> Error: {error.message}</h1>;
+  } else if (!isLoaded) {
+    return <h1> ...loading</h1>;
+  } else {
+    return (
+      <>
+        <h1>Top Stories</h1>
+        <ul>
+          {topStories.map((article, index) => (
+            <li key={index}>
+              <h3>{article.title}</h3>
+              <p>{article.abstract}</p>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
 
 export default TopStories;
